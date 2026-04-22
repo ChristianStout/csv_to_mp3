8ad712e0976c66191670ea91b0ea2dde1577b9f0
Christian Stout <christian.allen.stout@gmail.com>
2026-04-22 17:52:57 -0400

    Will now pass on errored songs and save to a file

--- csv_to_mp3.py
+++ csv_to_mp3.py
@@ -3,6 +3,11 @@
 '''
 # csv to mp3 #
 
+Original Author: (HarryMaher)[https://github.com/HarryMaher]
+Modification By: (ChristianStout)[https://github.com/ChristianStout], 04-22-2026
+
+# The following is the original README. Please read the updated version in `README.md`.
+
 ### What it does: ###
 
 This simple script takes a .csv list of popular song titles and artist names
@@ -54,9 +59,11 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import os
+import csv
 
 songs_csv = "songs.csv"
-cache = dict()
+cache: dict[str, set[str]] = dict()
+failed_csv_rows = ["song", "artist", "error"]
 
 def yt_dler(vid_link):
     # d/l settings
@@ -73,6 +80,12 @@ def yt_dler(vid_link):
     ydl = youtube_dl.YoutubeDL(ydl_opts)
     ydl.download([vid_link])
 
+def reg_failed_song(file, count, e, song, artist):
+    writer = csv.writer(file)
+    if count == 0:
+        writer.writerows(failed_csv_rows)
+    writer.writerows([song, artist, f'{e}'])
+
 def main():
     # Modification by ChristianStout - Require commandline args
     args = sys.argv[1:]
@@ -81,25 +94,17 @@ def main():
         print("Usage: csv_to_mp3 [CSV_FILE]")
         return
 
-    # 1. First argument must be the CSV path
+    # First argument must be the CSV path, or fail
     songs_csv = args[0]
-
-    # # 2. Second argument must be output DIRECTORY
-    # output_dir = args[1]
 
     if not os.path.isfile(songs_csv):
         print(f"Error: {songs_csv} is not a valid file")
         return
-    # if not os.path.isdir(output_dir):
-    #     print(f"Error: {output_dir} is not a valid directory")
-    #     return
-
-    # End Mod
 
     songs = pd.read_csv(songs_csv)
-    search_url = 'https://www.youtube.com/results?search_query='
-    yt_link = 'http://www.youtube.com'
     i = 0
+    failed_songs = open("failed_songs.csv", "w", newline='')
+    failed_count = 0
     while i < len(songs["song"]):
         # in case there are any repitions, we want to cache artist and song, and skip any we have already done
         artist = songs["artist"][i]
@@ -107,15 +112,27 @@ def main():
         if not artist in cache.keys():
             cache[artist] = set()
         if song in cache[artist]:
+            print("Duplicate found: {artist} - {song}. Skipping...")
             i += 1
             continue
         cache[artist].add(song)
 
         this_search = f"{artist} {song} official audio"
-
-        yt_dler(f'ytsearch:{this_search}')
+    
+        try:
+            yt_dler(f'ytsearch:{this_search}')
+        except Exception as e:
+            print("A song failed: {artist} - {song}. Skipping...")
+            reg_failed_song(failed_songs, failed_count, e, song, artist)
+            failed_count += 1
         i += 1
 
+    if failed_count > 0:
+        print("WARNING: Errors occurred during this process, and some songs were not downloaded.")
+        print(" The songs are saved to `failed_songs.csv` in this directory.")
+        print(" Please refer to the README for instruction on how to download manually.")
+
+    failed_songs.close()
 
 if __name__ == "__main__":
     main()
